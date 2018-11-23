0151aec3b1abfd0be42f8f970fb47437c5c5ed68
Xiangs18 <xiangs18@bu.edu>
2018-11-23 23:42:58 +0800

    Some changes

--- model_train/testing_code.py
+++ model_train/testing_code.py
@@ -1,6 +1,6 @@
 #import running_code
 print("It is triggered")
-from model_train import running_code 
-new_obj = running_code.hello_word("Emma", "Li")
+from model_train import running_code
+new_obj = running_code.hello_word("Stan", "Newsome")
 new_obj.introduce()
 print('-------------------')
